da181ed2eb982cecdff0741be6ef122c5b44bc85
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-07-18 20:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // write your code here const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+  
+  // Use the indexOf method to find the first occurrence
+  return lowerStr.indexOf(lowerSubStr);
+}
+
+// Examples
+console.log(indexOfIgnoreCase("Hello World", "world"));  // 6
+console.log(indexOfIgnoreCase("apple", "Ple"));          // 2
+console.log(indexOfIgnoreCase("test", "aaa"));           // -1
+
 }
 
 // Please do not change the code below
